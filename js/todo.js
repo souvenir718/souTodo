f6774528c492c49fb8f33b11ab45af301e1d518f
souvenir718 <mongkey0@naver.com>
2021-02-08 09:01:33 +0900

    투두 localStorage 통해 저장 기능 완.

--- js/todo.js
+++ js/todo.js
@@ -1,1 +1,67 @@
-const todoContainer = document.querySelector('.js-todo');
+const todoForm = document.querySelector('.js-todo'),
+    todoInput = todoForm.querySelector('.todo-input'),
+    todoList = todoForm.querySelector('.todo-list');
+const todoTotal = document.querySelector('.todo-total');
+const TODOS_LS = 'TODO_LIST';
+
+let todos = [];
+
+function saveTodo() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(todos));
+}
+
+function loadTodo() {
+    const strTodo = localStorage.getItem(TODOS_LS);
+
+    if (strTodo !== null) {
+        const parsedTodos = JSON.parse(strTodo);
+
+        parsedTodos.forEach(function (todo) {
+            paintTodo(todo.text);
+        });
+    }
+}
+
+function addTodo(event) {
+    event.preventDefault();
+    const todoText = todoInput.value;
+    paintTodo(todoText);
+    todoInput.value = '';
+}
+
+function completeTodo(event) {
+    const li = event.target.parentNode;
+    li.classList.toggle('check-todo');
+}
+
+function paintTodo(text) {
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    const input = document.createElement('input');
+    const newId = Math.random().toString(36).slice(2);
+
+    input.setAttribute('type', 'checkbox');
+    span.innerHTML = `${text} `;
+
+    li.appendChild(input);
+    li.appendChild(span);
+    li.id = newId;
+
+    input.addEventListener('change', completeTodo);
+
+    const newTodo = {
+        id: newId,
+        text,
+    };
+
+    todoList.appendChild(li);
+    todos.push(newTodo);
+    todoTotal.innerHTML = todos.length;
+    saveTodo();
+}
+
+function init() {
+    loadTodo();
+}
+todoForm.addEventListener('submit', addTodo);
+init();
